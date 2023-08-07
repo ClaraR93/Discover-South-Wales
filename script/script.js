6bbb3679a21aaeb51714ef63a98ea8228a484c31
ClaraR93 <>
2023-08-07 08:42:49 +0000

    Add empty functions to script.js to map out structure of game

---
+++ script/script.js
@@ -0,0 +1,24 @@
+//Function to run through questions and answers 
+function askQuestions() {
+
+}
+
+//Function to check if user has selected a radio button before proceeding
+function validateUserInput () {
+
+}
+
+//Function to check if user has selected the final radio button before submitting results
+function validateUserSubmission () {
+
+}
+
+//Function to store user inputs into an array
+function storeUserAnswers() {
+
+}
+
+//Function to run quiz, calling other functions when required
+function runQuiz() {
+
+}
