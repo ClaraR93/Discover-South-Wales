556e361b8ecc2a3f588aaecd8743d48da7d1647f
ClaraR93 <>
2023-08-19 08:14:04 +0000

    Add map marker for Rhossili in maps.js

--- script/maps.js
+++ script/maps.js
@@ -1,15 +1,26 @@
-//Google maps API function 
-//Code used from Google maps documentation - See readme credits
 let map;
 
 async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
+  // The location of Rhossili
+    const rhossili = { lat: 51.57882931112593, lng: -4.292492257856478 };  
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-    map = new Map(document.getElementById("maps-container"), {
-        center: { lat: 51.840674510742, lng: -3.7255938139578 },
-        zoom: 9,
-    });
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("maps-container"), {
+    zoom: 9,
+    center: { lat: 51.78009226078144, lng: - 3.9205687076682825},
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Rhossili
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: rhossili,
+    title: "Rhossili",
+  });
 }
 
 initMap();
-
