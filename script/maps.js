021b815f102409f0290cab018ccdfae6530d3b6a
ClaraR93 <>
2023-08-19 08:44:12 +0000

    Add 3 markers to google maps function in maps.js

--- script/maps.js
+++ script/maps.js
@@ -1,19 +1,16 @@
-// The following example creates five accessible and
-// focusable markers.
+// Google maps function, including 11 markers corresponding to each quiz result 
 function initMap() {
     const map = new google.maps.Map(document.getElementById("maps-container"), {
-        zoom: 12,
-        center: { lat: 34.84555, lng: -111.8035 },
+        zoom: 9,
+        center: { lat: 51.80044850212119, lng: - 3.7944135216672383 },  
     });
-    // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+    // Set LatLng and title text for the markers. The first marker (Rhossili)
     // receives the initial focus when tab is pressed. Use arrow keys to
     // move between markers; press tab again to cycle through the map controls.
     const tourStops = [
-        [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
-        [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
-        [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
-        [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
-        [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+        [{ lat: 51.579958806850016, lng: -4.291999384202944 }, "Rhossili"],
+        [{ lat: 51.418522478498495, lng: -3.576441918485766 }, "Monknash"],
+        [{ lat: 51.53696032335808, lng: -3.254880482895733 }, "Castle Coch Taff Trail"]
     ];
     // Create an info window to share between markers.
     const infoWindow = new google.maps.InfoWindow();
