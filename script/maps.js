5af28815849a9d5e4f78eff5d082fb0e78ea650e
ClaraR93 <>
2023-08-19 08:35:17 +0000

    Remove previous initMap function and replace with multiple marker boilerplate code in maps.js

--- script/maps.js
+++ script/maps.js
@@ -1,26 +1,40 @@
-let map;
+// The following example creates five accessible and
+// focusable markers.
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("maps-container"), {
+        zoom: 12,
+        center: { lat: 34.84555, lng: -111.8035 },
+    });
+    // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+    // receives the initial focus when tab is pressed. Use arrow keys to
+    // move between markers; press tab again to cycle through the map controls.
+    const tourStops = [
+        [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
+        [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
+        [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
+        [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
+        [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+    ];
+    // Create an info window to share between markers.
+    const infoWindow = new google.maps.InfoWindow();
 
-async function initMap() {
-  // The location of Rhossili
-    const rhossili = { lat: 51.57882931112593, lng: -4.292492257856478 };  
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    // Create the markers.
+    tourStops.forEach(([position, title], i) => {
+        const marker = new google.maps.Marker({
+            position,
+            map,
+            title: `${i + 1}. ${title}`,
+            label: `${i + 1}`,
+            optimized: false,
+        });
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("maps-container"), {
-    zoom: 9,
-    center: { lat: 51.78009226078144, lng: - 3.9205687076682825},
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Rhossili
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: rhossili,
-    title: "Rhossili",
-  });
+        // Add a click listener for each marker, and set up the info window.
+        marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(marker.getTitle());
+            infoWindow.open(marker.getMap(), marker);
+        });
+    });
 }
 
-initMap();
+window.initMap = initMap;
