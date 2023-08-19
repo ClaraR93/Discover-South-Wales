0db3ac9ae78fe40d881e331851f7e7b56167ccbe
ClaraR93 <>
2023-08-19 10:34:20 +0000

    Add URL's to corresponding southWalesLocations array markers

--- script/maps.js
+++ script/maps.js
@@ -9,16 +9,16 @@ function initMap() {
     // move between markers; press tab again to cycle through the map controls.
     const southWalesLocations = [
         [{ lat: 51.579958806850016, lng: -4.291999384202944 }, "Rhossili", "https://www.visitswanseabay.com/inspirations/beach-guide/rhossili-bay-beach/"],
-        [{ lat: 51.418522478498495, lng: -3.576441918485766 }, "Monknash"],
-        [{ lat: 51.53696032335808, lng: -3.254880482895733 }, "Castle Coch Taff Trail"],
-        [{ lat: 51.69749376467262, lng: -4.683001462694947 }, "Saundersfoot to Monkstone point"],
-        [{ lat: 51.77730017586767, lng: - 3.587275105343132 }, "Sgwd ddwli uchaf Waterfall Walk"],
-        [{ lat: 51.8772044249596, lng: - 3.4011289190848886 }, "Fan Y Big"],
-        [{ lat: 51.86673591823695, lng: - 3.3819958359916416 }, "Gwaun Cerrig Llwydion"],
-        [{ lat: 51.884470931881985, lng: - 3.436356918503445 }, "Pen Y Fan"],
-        [{ lat: 51.771770210073846, lng: - 3.610112844138401 }, "Sgwd Einnion Gam (Follow Elidir Trail)"],
-        [{ lat: 51.86396709184166, lng: - 3.4991761782958357 }, "Fan Fawr"],
-        [{ lat: 51.840222514942944, lng: - 3.382304129256682 }, "Horseshow Ridge Walk"]
+        [{ lat: 51.418522478498495, lng: -3.576441918485766 }, "Monknash", "https://discovertheoutdoors.co.uk/monknash-beach/"],
+        [{ lat: 51.53696032335808, lng: -3.254880482895733 }, "Castle Coch Taff Trail", "http://edtechie.net/tonwalks/castell-coch/castell-coch-taff-trail/"],
+        [{ lat: 51.69749376467262, lng: -4.683001462694947 }, "Saundersfoot to Monkstone point", "https://www.visitpembrokeshire.com/explore-pembrokeshire/beaches/monkstone"],
+        [{ lat: 51.77730017586767, lng: - 3.587275105343132 }, "Sgwd ddwli uchaf Waterfall Walk", "https://www.waymarking.com/waymarks/WMA9ZE_Sgwd_Ddwli_Uchaf_Upper_Gushing_Falls_Pontneddfechan_Powys_Wales"],
+        [{ lat: 51.8772044249596, lng: - 3.4011289190848886 }, "Fan Y Big", "https://www.walkmyworld.com/posts/fan-y-big"],
+        [{ lat: 51.86673591823695, lng: - 3.3819958359916416 }, "Gwaun Cerrig Llwydion", "https://www.themountainguide.co.uk/wales/gwaun-cerrig-llwydion.htm"],
+        [{ lat: 51.884470931881985, lng: - 3.436356918503445 }, "Pen Y Fan", "https://www.visitwales.com/things-do/adventure-and-activities/walking/crowning-glory-4-ways-walk-pen-y-fan"],
+        [{ lat: 51.771770210073846, lng: - 3.610112844138401 }, "Sgwd Einnion Gam (Follow Elidir Trail)", "https://ohwhataknight.co.uk/blog/how-to-reach-sgwd-einion-gam-waterfall"],
+        [{ lat: 51.86396709184166, lng: - 3.4991761782958357 }, "Fan Fawr", "https://www.alltrails.com/en-gb/trail/wales/powys/fan-fawr-and-beacons-reservoir"],
+        [{ lat: 51.840222514942944, lng: - 3.382304129256682 }, "Horseshow Ridge Walk", "https://www.nationaltrust.org.uk/visit/wales/brecon-beacons/brecon-beacons-horseshoe-ridge-walk"]
         
     ];
     // Create an info window to share between markers.
@@ -42,6 +42,7 @@ function initMap() {
         });
 
         // Add a click listener to open the specified URL when the marker is clicked.
+        //Used window.open() method as explained in Stack overflows - See readme credits
         marker.addListener("click", () => {
             window.open(url, "_blank");
         });
