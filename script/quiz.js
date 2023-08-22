f81041a8ab1e0920b04bc8edfd48e88fd2f4cc3d
ClaraR93 <>
2023-08-22 11:38:44 +0000

    Proof read the quizAnswers array in quiz.js

--- script/quiz.js
+++ script/quiz.js
@@ -9,7 +9,7 @@ const questions = [
     },
     {
         question: "How do you feel about heights and steep terrains?",
-        answer: ["Not a fan; prefer flat and low-altitude landscapes", "I'm okay with some heights and moderate inclines", "Love them! Bring on the adrenaline and spectacular views"],
+        answer: ["Not a fan; prefer flat landscapes", "I'm okay with some heights and moderate inclines", "Love them! Bring on the adrenaline and spectacular views"],
     },
     {
         question: "How much time are you willing to spend on the activity?",
@@ -48,13 +48,13 @@ const quizAnswers = [
     },
     {
         name: "Saundersfoot to Monkstone point",
-        description: "Walk along the cliff side through the woods, or wait for low tide and take an easier stroll on the beach. Arriving at Monkstone point you will be greeted with breathtaking views of both Saundersfoot and Temby from either side. Enjoy the secluded beach Monkstone has to offer as well",
+        description: "Walk along the cliff side through the woods, or wait for low tide and take an easier stroll on the beach. Arriving at Monkstone point you will be greeted with breathtaking views of both Saundersfoot and Tenby from either side. Enjoy the secluded beach Monkstone has to offer as well",
         image: "assets/images/results/saundersfoot.jpg",
         url: "https://www.visitpembrokeshire.com/explore-pembrokeshire/beaches/monkstone"
     },
     {
         name: "Sgwd Ddwli Uchaf Waterfall",
-        description: "The Sgwd Ddwli Uchaf is located on the Nedd Fechan and is one of three waterfalls along this section of the river. The falls has a drop of about 15 feet. It is best to approach the falls from the bottom which will entail leaving the main path a short distance from the falls.",
+        description: "This waterfall is located on the Nedd Fechan and is one of three waterfalls along this section of the river. The falls has a drop of about 15 feet. It is best to approach this from the bottom which will entail leaving the main path a short distance from the falls.",
         image: "assets/images/results/sgwd-ddwli-uchaf-waterfall.jpg",
         url: "https://www.waymarking.com/waymarks/WMA9ZE_Sgwd_Ddwli_Uchaf_Upper_Gushing_Falls_Pontneddfechan_Powys_Wales"
     },
@@ -72,25 +72,25 @@ const quizAnswers = [
     },
     {
         name: "Pen Y Fan",
-        description: "Pen y Fan is the highest mountain in southern Britain. Deservedly popular with walkers, the views from the summit are truly incredible. Why not take a hike up at sunrise on a clear day, you will be rewarded for your efforts greatly",
+        description: "Pen y Fan is the highest mountain in southern Britain. Deservedly popular with walkers, the views from the summit are truly incredible. Why not take a hike up at sunrise on a clear day, you will be rewarded for your early start!",
         image: "assets/images/results/pen-y-fan.jpg",
         url: "https://www.visitwales.com/things-do/adventure-and-activities/walking/crowning-glory-4-ways-walk-pen-y-fan"
     },
     {
         name: "Elidir Trail secret waterfall",
-        description: "A spellbinding in the Brecons, this trail offers endless crystal clear swimming holes and even a secret waterfall hidden off track. If you're up for the challenge to reach this, be mindful that you will have to cross the river twice and scamble up an offbeaten track to reach it, but its well worth the challenge.",
+        description: "A spellbinding walk in the Brecons, this trail offers endless crystal clear swimming holes and even a secret waterfall hidden off track. If you're up for the challenge to reach this, be mindful that you will have to cross the river twice and scamble up an offbeaten track to reach it, but its well worth the challenge.",
         image: "assets/images/results/elidir-trail-waterfall.jpg",
         url: "https://ohwhataknight.co.uk/blog/how-to-reach-sgwd-einion-gam-waterfall"
     },
     {
         name: "Fan Fawr",
-        description: "Enjoy this 7.7-km circular trail near Brecon, Powys. Generally considered a moderately challenging route, it takes an average of 2 h 40 min to complete. This trail is great for hiking, running, and walking, and it's unlikely you'll encounter many other people while exploring.",
+        description: "Enjoy this fun circular trail near Brecon, Powys. Generally considered a moderately challenging route, this trail is great for hiking, running, and walking, and it's unlikely you'll encounter many other people while exploring.",
         image: "assets/images/results/fan-fawr.jpg",
         url: "https://www.alltrails.com/en-gb/trail/wales/powys/fan-fawr-and-beacons-reservoir"
     },
     {
         name: "Horseshoe Ridge",
-        description: "If you're after a challenging but rewarding walk across wild Welsh moorland and up steep rocky peaks that offer commanding views over the Brecon Beacons, the Horseshoe Ridge hike is for you. At every step of the circular walk around the U-shaped Neuadd Valley and up the four peaks of Corn Du, Pen y Fan (the highest peak in southern Britain), Cribyn and Fan y Big, you'll be swept away by magnificent views stretching for miles in all directions.",
+        description: "If you're after a challenging but rewarding walk across wild Welsh moorland and up steep rocky peaks, offering commanding views over the Brecon Beacons, the Horseshoe Ridge hike is for you. At every step of the circular walk around the U-shaped Neuadd Valley and up the four peaks of Corn Du, Pen y Fan (the highest peak in southern Britain), Cribyn and Fan y Big, you'll be swept away by magnificent views stretching for miles in all directions.",
         image: "assets/images/results/horseshoe-ridge-walk.jpg",
         url: "https://www.nationaltrust.org.uk/visit/wales/brecon-beacons/brecon-beacons-horseshoe-ridge-walk"
     }
